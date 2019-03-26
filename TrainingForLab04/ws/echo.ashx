fc4f1f0e4be99e8e26dcc9a1535a67dbccc12d28
Carlos Gallego <gallegohh@outlook.com>
2019-03-26 21:31:57 +0100

    Web Socket example added

---
+++ TrainingForLab04/ws/echo.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="echo.ashx.cs" Class="TrainingForLab04.ws.echo" %>
---
+++ TrainingForLab04/ws/echo.ashx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// NuGet -> Microsoft.WebSockets
+using Microsoft.Web.WebSockets;
+
+namespace TrainingForLab04.ws
+{
+    public class echo : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(new WebSocket());
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+
+    public class WebSocket : WebSocketHandler
+    {
+        public override void OnMessage(string message)
+        {
+            this.Send(">" + message);
+        }
+
+        public override void OnOpen()
+        {
+            this.Send("CONECTADO");
+        }
+
+        public override void OnClose()
+        {
+            this.Send("DESCONECTADO");
+        }
+
+        public override void OnError()
+        {
+            this.Send("Algo malo ha ocurrido");
+        }
+    }
+}
